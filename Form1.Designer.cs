9cdc50b48af66aa7a669baba5d60f8bbb5a34ab4
DragusinMadalinIonut <dragusin.madalin.ionut@gmail.com>
2023-05-21 06:53:06 -0900

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox titleBox;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox notesBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView previousNotes;
+        private System.Windows.Forms.Button loadBTN;
+        private System.Windows.Forms.Button deleteBTN;
+        private System.Windows.Forms.Button newBTN;
+        private System.Windows.Forms.Button saveBTN;
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.titleBox = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.notesBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.previousNotes = new System.Windows.Forms.DataGridView();
+            this.loadBTN = new System.Windows.Forms.Button();
+            this.deleteBTN = new System.Windows.Forms.Button();
+            this.newBTN = new System.Windows.Forms.Button();
+            this.saveBTN = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.previousNotes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titleBox
+            // 
+            this.titleBox.Location = new System.Drawing.Point(392, 66);
+            this.titleBox.Name = "titleBox";
+            this.titleBox.Size = new System.Drawing.Size(511, 22);
+            this.titleBox.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // notesBox
+            // 
+            this.notesBox.Location = new System.Drawing.Point(404, 173);
+            this.notesBox.Multiline = true;
+            this.notesBox.Name = "notesBox";
+            this.notesBox.Size = new System.Drawing.Size(511, 452);
+            this.notesBox.TabIndex = 2;
+            this.notesBox.TextChanged += new System.EventHandler(this.notesBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(387, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(516, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Titlu:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(399, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(516, 44);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nota:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // previousNotes
+            // 
+            this.previousNotes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.previousNotes.Location = new System.Drawing.Point(12, 30);
+            this.previousNotes.Name = "previousNotes";
+            this.previousNotes.RowHeadersWidth = 51;
+            this.previousNotes.RowTemplate.Height = 24;
+            this.previousNotes.Size = new System.Drawing.Size(369, 498);
+            this.previousNotes.TabIndex = 6;
+            this.previousNotes.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.previousNotes_CellContentClick);
+            // 
+            // loadBTN
+            // 
+            this.loadBTN.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loadBTN.Location = new System.Drawing.Point(12, 549);
+            this.loadBTN.Name = "loadBTN";
+            this.loadBTN.Size = new System.Drawing.Size(161, 41);
+            this.loadBTN.TabIndex = 7;
+            this.loadBTN.Text = "Load";
+            this.loadBTN.UseVisualStyleBackColor = true;
+            this.loadBTN.Click += new System.EventHandler(this.loadBTN_Click);
+            // 
+            // deleteBTN
+            // 
+            this.deleteBTN.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deleteBTN.Location = new System.Drawing.Point(202, 549);
+            this.deleteBTN.Name = "deleteBTN";
+            this.deleteBTN.Size = new System.Drawing.Size(166, 41);
+            this.deleteBTN.TabIndex = 8;
+            this.deleteBTN.Text = "Delete";
+            this.deleteBTN.UseVisualStyleBackColor = true;
+            this.deleteBTN.Click += new System.EventHandler(this.deleteBTN_Click);
+            // 
+            // newBTN
+            // 
+            this.newBTN.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newBTN.Location = new System.Drawing.Point(12, 603);
+            this.newBTN.Name = "newBTN";
+            this.newBTN.Size = new System.Drawing.Size(161, 41);
+            this.newBTN.TabIndex = 9;
+            this.newBTN.Text = "New";
+            this.newBTN.UseVisualStyleBackColor = true;
+            this.newBTN.Click += new System.EventHandler(this.newBTN_Click);
+            // 
+            // saveBTN
+            // 
+            this.saveBTN.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveBTN.Location = new System.Drawing.Point(202, 603);
+            this.saveBTN.Name = "saveBTN";
+            this.saveBTN.Size = new System.Drawing.Size(166, 41);
+            this.saveBTN.TabIndex = 10;
+            this.saveBTN.Text = "Save";
+            this.saveBTN.UseVisualStyleBackColor = true;
+            this.saveBTN.Click += new System.EventHandler(this.saveBTN_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 716);
+            this.Controls.Add(this.saveBTN);
+            this.Controls.Add(this.newBTN);
+            this.Controls.Add(this.deleteBTN);
+            this.Controls.Add(this.loadBTN);
+            this.Controls.Add(this.previousNotes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.notesBox);
+            this.Controls.Add(this.titleBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.previousNotes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        
+
+        private void previousNotes_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0 && previousNotes.Columns[e.ColumnIndex].Name == "Content")
+            {
+                previousNotes.CurrentCell = previousNotes.Rows[e.RowIndex].Cells[e.ColumnIndex];
+                loadBTN_Click(sender, e);
+
+                notesBox.ReadOnly = false;
+            }
+        }
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "Data Source=DESKTOP-1LE57OT;Initial Catalog=DB;Integrated Security=True;";
+        private SqlConnection connection;
+        private SqlDataAdapter dataAdapter;
+        private DataTable dataTable;
+
+        private void notesBox_TextChanged(object sender, EventArgs e)
+        {
+            if (previousNotes.SelectedRows.Count > 0)
+            {
+                int selectedIndex = previousNotes.SelectedRows[0].Index;
+                int noteId = Convert.ToInt32(previousNotes.Rows[selectedIndex].Cells["NoteID"].Value);
+
+                dataTable.Rows[selectedIndex]["Content"] = notesBox.Text; // Update the Content in the DataTable
+                dataTable.Rows[selectedIndex]["Title"] = titleBox.Text; // Update the Title in the DataTable
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (VerifyConnection())
+                {
+                    dataAdapter.Update(dataTable); // Save changes to the database
+                    MessageBox.Show("Changes saved successfully!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error occurred while saving changes: " + ex.Message);
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                    connection.Close();
+            }
+            LoadNotes(); // Reload the data to refresh the DataGridView
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            connection = new SqlConnection(connectionString);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadNotes();
+
+            // Set font properties for notesBox TextBox
+            notesBox.Font = new System.Drawing.Font("Arial", 12, System.Drawing.FontStyle.Regular);
+
+            // Check if previousNotes DataGridView has columns
+            if (previousNotes.Columns.Count > 0)
+            {
+                // Set font properties for specific columns
+                previousNotes.Columns["Title"].DefaultCellStyle.Font = new System.Drawing.Font("Calibri", 10, System.Drawing.FontStyle.Bold);
+                previousNotes.Columns["Content"].DefaultCellStyle.Font = new System.Drawing.Font("Calibri", 10, System.Drawing.FontStyle.Regular);
+            }
+        }
+
+        private void LoadNotes()
+        {
+            try
+            {
+                if (VerifyConnection())
+                {
+                    string selectQuery = "SELECT NoteID, Title, Content FROM Notes";
+                    dataAdapter = new SqlDataAdapter(selectQuery, connection);
+                    dataTable = new DataTable();
+                    dataAdapter.Fill(dataTable);
+
+                    previousNotes.DataSource = dataTable;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error occurred while loading notes: " + ex.Message);
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                    connection.Close();
+            }
+        }
+
+        private void loadBTN_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (previousNotes.SelectedRows.Count > 0)
+                {
+                    int selectedIndex = previousNotes.SelectedRows[0].Index;
+                    int noteId = Convert.ToInt32(previousNotes.Rows[selectedIndex].Cells["NoteID"].Value);
+
+                    DataRow[] selectedRows = dataTable.Select($"NoteID = {noteId}");
+                    if (selectedRows.Length > 0)
+                    {
+                        titleBox.Text = selectedRows[0]["Title"].ToString();
+                        notesBox.Text = selectedRows[0]["Content"].ToString();
+                        MessageBox.Show("Note loaded successfully!");
+                    }
+                    else
+                    {
+                        titleBox.Text = string.Empty;
+                        notesBox.Text = string.Empty;
+                        MessageBox.Show("Failed to load note.");
+                    }
+                }
+                else
+                {
+                    titleBox.Text = string.Empty;
+                    notesBox.Text = string.Empty;
+                    MessageBox.Show("Please select a note to load.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error occurred while loading note: " + ex.Message);
+            }
+        }
+
+        private void previousNotes_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            int rowIndex = e.RowIndex;
+            DataGridViewRow editedRow = previousNotes.Rows[rowIndex];
+            int noteId = Convert.ToInt32(editedRow.Cells["NoteID"].Value);
+
+            DataRow[] selectedRows = dataTable.Select($"NoteID = {noteId}");
+            if (selectedRows.Length > 0)
+            {
+                selectedRows[0]["Title"] = editedRow.Cells["Title"].Value;
+                selectedRows[0]["Content"] = editedRow.Cells["Content"].Value;
+            }
+        }
+
+        private bool VerifyConnection()
+        {
+            try
+            {
+                connection.Open();
+                connection.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to connect to the database: " + ex.Message);
+                return false;
+            }
+        }
+
+        private void deleteBTN_Click(object sender, EventArgs e)
+        {
+            if (previousNotes.SelectedRows.Count > 0)
+            {
+                int selectedIndex = previousNotes.SelectedRows[0].Index;
+                int noteId = Convert.ToInt32(previousNotes.Rows[selectedIndex].Cells["NoteID"].Value);
+
+                try
+                {
+                    if (VerifyConnection())
+                    {
+                        string deleteQuery = "DELETE FROM Notes WHERE NoteID = @NoteID";
+                        SqlCommand deleteCommand = new SqlCommand(deleteQuery, connection);
+                        deleteCommand.Parameters.AddWithValue("@NoteID", noteId);
+
+                        connection.Open();
+                        deleteCommand.ExecuteNonQuery();
+                        connection.Close();
+
+                        MessageBox.Show("Note deleted successfully!");
+
+                        LoadNotes();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error occurred while deleting the note: " + ex.Message);
+                }
+                finally
+                {
+                    if (connection.State == ConnectionState.Open)
+                        connection.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a note to delete.");
+            }
+        }
+
+        private void newBTN_Click(object sender, EventArgs e)
+        {
+            titleBox.Text = string.Empty;
+            notesBox.Text = string.Empty;
+        }
+
+        private void saveBTN_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (VerifyConnection())
+                {
+                    if (previousNotes.SelectedRows.Count > 0)
+                    {
+                        int selectedIndex = previousNotes.SelectedRows[0].Index;
+                        int noteId = Convert.ToInt32(previousNotes.Rows[selectedIndex].Cells["NoteID"].Value);
+
+                        // Update the title in the DataTable
+                        dataTable.Rows[selectedIndex]["Title"] = titleBox.Text;
+
+                        string updateQuery = "UPDATE Notes SET Title = @Title, Content = @Content WHERE NoteID = @NoteID";
+                        SqlCommand updateCommand = new SqlCommand(updateQuery, connection);
+                        updateCommand.Parameters.AddWithValue("@Title", titleBox.Text);
+                        updateCommand.Parameters.AddWithValue("@Content", notesBox.Text);
+                        updateCommand.Parameters.AddWithValue("@NoteID", noteId);
+
+                        connection.Open();
+                        updateCommand.ExecuteNonQuery();
+                        connection.Close();
+
+                        MessageBox.Show("Note updated successfully!");
+                    }
+                    else
+                    {
+                        string insertQuery = "INSERT INTO Notes (Title, Content) VALUES (@Title, @Content)";
+                        SqlCommand insertCommand = new SqlCommand(insertQuery, connection);
+                        insertCommand.Parameters.AddWithValue("@Title", titleBox.Text);
+                        insertCommand.Parameters.AddWithValue("@Content", notesBox.Text);
+
+                        connection.Open();
+                        insertCommand.ExecuteNonQuery();
+                        connection.Close();
+
+                        MessageBox.Show("Note saved successfully!");
+                    }
+
+                    LoadNotes();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error occurred while saving the note: " + ex.Message);
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                    connection.Close();
+            }
+        }
+    }
+}
